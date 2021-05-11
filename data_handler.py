a4f7742f016efaa7df762dff6e65800595797dc9
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-05-11 15:10:08 +0300

    modified ask a question

--- data_handler.py
+++ data_handler.py
@@ -5,6 +5,7 @@ import time
 FILE_QUESTIONS = 'sample_data/question.csv'
 FILE_ANSWERS = 'sample_data/answer.csv'
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title' , 'message', 'image']
+DATA_HEADER_ANSWERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
 def get_all_data_from_questions():
@@ -30,8 +31,7 @@ def get_question_data(question_id, options):
     return result_list
 
 
-
-def get_question_answers(question_id):
+def get_question_content(question_id):
     with open(FILE_QUESTIONS, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
         for line in reader:
@@ -40,22 +40,66 @@ def get_question_answers(question_id):
     return answers_list
 
 
-def write_user_question(question):
+def write_user_question(question_list):
     question_data = []
-    questions_data_list = get_all_data_from_question() 
+    questions_data_list = get_all_data_from_questions()
     max = 0
-    for element in question_data_list:
+    for element in questions_data_list:
         if int(element['id']) > max:
             max = int(element['id'])
     question_data.append(max + 1)
-    question_data.append(round(time.time() * 1000)
-    print(question_data)
+    question_data.append(round(time.time()))
     question_data.append(0)
     question_data.append(0)
-    question_data.append(question)
+    question_data.append(question_list[0])
+    question_data.append(question_list[1])
     with open(FILE_QUESTIONS, 'a', newline = '') as csv_file:
         fieldnames = DATA_HEADER
-        writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         data_dict = dict(zip(DATA_HEADER, question_data))
         writer.writerow(data_dict)
+    return question_data[0]
+
+
+def get_answers(question_id):
+    answers_list = get_all_data_from_answers()
+    result_list = []
+    for element in answers_list:
+        if question_id == element['question_id']:
+            result_list.append(element['message'])
+    return result_list
+
+
+def get_all_data_from_answers():
+    answers_list = []
+    with open(FILE_ANSWERS, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            timestamp = int(row['submission_time'])
+            date_time = datetime.fromtimestamp(timestamp)
+            row['submission_time'] = date_time
+            answers_list.append(row)
+    return answers_list
+
+
+def write_answer(question_id, answer):
+    answer_data = []
+    questions_data_list = get_all_data_from_answers()
+    max = 0
+    for element in questions_data_list:
+        if int(element['id']) > max:
+            max = int(element['id'])
+    answer_data.append(max + 1)
+    answer_data.append(round(time.time()))
+    answer_data.append(0)
+    answer_data.append(0)
+    answer_data.append(question_id)
+    answer_data.append(answer)
+    with open(FILE_ANSWERS, 'a', newline = '') as csv_file:
+        fieldnames = DATA_HEADER_ANSWERS
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        data_dict = dict(zip(DATA_HEADER_ANSWERS, answer_data))
+        writer.writerow(data_dict)
+    return answer_data[0]
+
 
--- server.py
+++ server.py
@@ -9,17 +9,6 @@ def route_home():
     return render_template('home.html')
 
 
-@app.route('/add-question', methods=['GET', 'POST'])
-def add_new_question():
-    question = ''
-    if request.method == 'POST':
-        form_dict = request.form
-        for value in form_dict.values():
-            question = value
-        data_handler.write_user_story(question)
-        return redirect(url_for('route_list'))
-
-
 @app.route("/list")
 def route_list():
     questions = data_handler.get_all_data_from_questions()
@@ -28,9 +17,39 @@ def route_list():
 
 @app.route("/question/<question_id>")
 def route_question(question_id):
-    question_data = data_handler.get_question_answers(question_id)
+    question_data = data_handler.get_question_content(question_id)
     title = data_handler.get_question_data(question_id, 'title')
-    return render_template('question.html', question=question_id, question_data=question_data, title=title)
+    answers_data = data_handler.get_answers(question_id)
+    return render_template('question.html', question=question_id, question_data=question_data, title=title, answers_data=answers_data )
+
+
+@app.route('/add-question', methods=['GET', 'POST'])
+def add_new_question():
+    question_list = []
+    if request.method == 'POST':
+        form_dict = request.form
+        for value in form_dict.values():
+            question_list.append(value)
+        question_id = data_handler.write_user_question(question_list)
+        return redirect(url_for('route_question', question_id=question_id))
+    return render_template('add_question.html')
+
+
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
+def add_new_answer(question_id):
+    answer_list = []
+    if request.method == 'POST':
+        form_dict = request.form
+        for value in form_dict.values():
+            answer_list.append(value)
+        new_answer_id = data_handler.write_answer(question_id, answer_list)
+        # post answer form html
+        return redirect(url_for('route_question', question_id=question_id))
+    
+    return render_template('route_question')
+
+
+
 
 
 if __name__ == "__main__":
